21571eee64d1073f9cde1c8fef82c4cd7e941f4b
AntonP <solidscorpion@gmail.com>
2021-09-27 12:26:11 +0300

    Added java doc

--- app/src/main/java/com/apripachkin/tuneinbrowser/utils/image/ImageLoader.kt
+++ app/src/main/java/com/apripachkin/tuneinbrowser/utils/image/ImageLoader.kt
@@ -2,6 +2,10 @@ package com.apripachkin.tuneinbrowser.utils.image
 
 import android.widget.ImageView
 
+/**
+ * Image loader interface to hide implementation details and to allow swift switching of image
+ * loading libraries
+ */
 interface ImageLoader {
     fun loadImageInto(imageView: ImageView, url: String)
     fun clearImageView(imageView: ImageView)
