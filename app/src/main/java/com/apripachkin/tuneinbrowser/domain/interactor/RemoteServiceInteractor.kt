aeb802fc7238834908817d81a21b04854484d2eb
AntonP <solidscorpion@gmail.com>
2021-09-27 12:24:12 +0300

    Added java doc for interactor

--- app/src/main/java/com/apripachkin/tuneinbrowser/domain/interactor/RemoteServiceInteractor.kt
+++ app/src/main/java/com/apripachkin/tuneinbrowser/domain/interactor/RemoteServiceInteractor.kt
@@ -7,8 +7,25 @@ import com.apripachkin.tuneinbrowser.domain.repo.RemoteRepository
 import javax.inject.Inject
 
 interface RemoteServiceInteractor {
+    /**
+     * For loading main page categories
+     * Data gets mapped into domain UiItem
+     */
     suspend fun loadBasePage(): List<UiItem>
+
+    /** Method to load new data based from url returned by api
+     * Data gets mapped into wrapped class
+     * @param url URL returned from api to use for fetching next data
+     * @return wrapped class containing optional title for toolbar and list of new data to show
+     */
     suspend fun loadRemoteUrl(url: String): UiData
+
+    /**
+     * For loading audio links data
+     * Data is mapped as url to be used for streaming
+     * @param audio link data to load
+     * @return radio station stream URL to be used for streaming
+     */
     suspend fun loadAudioUrl(url: String): String
 }
 
